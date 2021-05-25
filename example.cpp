01f150f508c627f2bc8200fa6e69bb1984af27b9
Thane784 <kirillstepnov8@google.com>
2021-05-25 21:51:56 +0300

    Work

--- example.cpp
+++ example.cpp
@@ -3,16 +3,10 @@
 #include "myvector.hpp"
 
 int main(){
-    std::vector<int> vector{0,0,0,0};
-    auto itend{vector.begin()-10000};
-    auto it{vector.begin()};
+    myvector<int> myvec{0,0,0,0};
+    myiterator<int> it{myvec.begin()};
+    std::cout << *it <<"\n";
     myvector<int> vec{5,6,7,8,9,784};
-    for (;it>itend;--it){
-        if(*it!=0){
-            std::cout << *it << std::endl;
-            *it = 197;
-        }
-    }
     std::cout << vec << "\n";
     return(0);
 }
--- myiterator.hpp
+++ myiterator.hpp
@@ -1,29 +1,67 @@
 #pragma once
+#include <iterator>
 #include "myvector.hpp"
 
 template<class T>
 class myvector;
 
 template<class V>
-class myiterator: public std::iterator<std::random_access_iterator_tag, V>{//ttc write
+class myiterator: std::iterator<std::random_access_iterator_tag, V>{//ttc write
     friend class myvector<V>;
 private:
     V* m_ptr;
     myiterator(V *ptr) :m_ptr(ptr){};
 public:
+    myiterator():m_ptr(nullptr){};
     myiterator(const myiterator& it) :m_ptr(it.m_ptr){};
-    myiterator<V>::reference operator*() const {return(*m_ptr);};
-    myiterator<V>::reference operator*(){return(*m_ptr);};
+    myiterator(myiterator&& it);
+    myiterator<V>& operator =(const myiterator<V> & it);
+    myiterator<V>& operator =(myiterator<V>&& it);
+    myiterator::reference operator*() const {return(*m_ptr);};
+    myiterator::reference operator*(){return(*m_ptr);};
     myiterator<V> operator++(); 
     myiterator<V> operator++(V);
     myiterator<V> operator--();
     myiterator<V> operator--(V);
-    bool operator==(myiterator const& other) const;
+    bool operator==(myiterator const& other) const {return(m_ptr == other.m_ptr);};
+    bool operator!=(myiterator const& other) const {return(m_ptr != other.m_ptr);};
+    myiterator::reference operator[](ptrdiff_t n) const;
 };
 
+/*
+template<class V>
+myiterator<V>::reference myiterator<V>::operator[](myiterator::difference_type n) const{
+}
+*/
+
+template<class V>
+myiterator<V>::reference myiterator<V>::operator[](ptrdiff_t n) const{
+    auto temp = *this;
+    //temp += n; //ttc write
+    return(temp);
+}
+
 template<typename V>
-bool myiterator<V>::operator==(myiterator const& other) const{
-    return(m_ptr == other.m_ptr);
+myiterator<V>& myiterator<V>::operator =(myiterator<V> && it){
+    if (&it == this)
+		return *this;
+    m_ptr = it.m_ptr;
+    it.m_ptr = nullptr;
+    return(*this);
+}
+
+template<typename V>
+myiterator<V>& myiterator<V>::operator =(const myiterator<V> & it){
+    if (&it == this)
+		return *this;
+    m_ptr = it.m_ptr;
+    return(*this);
+}
+
+template<typename V>
+myiterator<V>::myiterator(myiterator&& it){
+    m_ptr = it.m_ptr; 
+    it.m_ptr = nullptr;
 }
 
 template<typename V>
